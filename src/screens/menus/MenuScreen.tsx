576a2386cd5be0a5db40af8829c487c62f30a543
DEVFATH <40345474+dev-fath@users.noreply.github.com>
2022-01-06 18:02:38 +0900

    Revert "[app] 페이지 내비게이터 추가 (#2)"
    This reverts commit bc9873b8b0f4ac5901ec5e84e2fbdbfadfc5c9ec.

--- src/screens/menus/MenuScreen.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Text} from 'react-native';
-
-function MenuScreen() {
-  return <Text>메뉴 스크린</Text>;
-}
-
-export default MenuScreen;
--- src/shared/constants/NavigatorTypes.ts
+++
@@ -1,8 +0,0 @@
-import {NativeStackScreenProps} from 'react-native-screens/native-stack';
-
-export type RootStackParamList = {
-  Home: undefined;
-  Menu: undefined;
-};
-
-export type HomeProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
